9e139911aac865f650836933723f5f27c4b4108b
Justin Narayan <justin1narayan@gmail.com>
2026-01-28 13:44:37 -0500

    Unit Test Framework

---
+++ lab2/unit_tests/task1.py
@@ -0,0 +1,30 @@
+import numpy as np
+from ..nodes.l2_planning import PathPlanner
+
+### Set up Path Planner
+# map info
+map_filename = "willowgarageworld_05res.png"
+map_setings_filename = "willowgarageworld_05res.yaml"
+# robot information
+goal_point = np.array([[10], [10]])  # m
+stopping_dist = 0.5  # m
+# create planner
+path_planner = PathPlanner(
+    map_filename, map_setings_filename, goal_point, stopping_dist
+)
+
+
+### Point to Cell
+
+# ...
+path_planner.point_to_cell([0,0])
+
+# ...
+path_planner.point_to_cell([1,1])
+
+
+
+### Points to Robot Circle
+
+# ...
+path_planner.points_to_robot_circle([ [0,0], [1,0] ])
---
+++ lab2/unit_tests/task2.py
@@ -0,0 +1,37 @@
+import numpy as np
+from ..nodes.l2_planning import PathPlanner
+
+### Set up Path Planner
+# map info
+map_filename = "willowgarageworld_05res.png"
+map_setings_filename = "willowgarageworld_05res.yaml"
+# robot information
+goal_point = np.array([[10], [10]])  # m
+stopping_dist = 0.5  # m
+# create planner
+path_planner = PathPlanner(
+    map_filename, map_setings_filename, goal_point, stopping_dist
+)
+
+
+### Trajectory Rollout
+
+# ...
+path_planner.trajectory_rollout( 0.5, 0.1 )
+
+# ...
+path_planner.trajectory_rollout( 0.1, 0.2 )
+
+
+
+### Robot Controller
+
+# ...
+path_planner.robot_controller( 0, np.array([0,0]) )
+
+
+
+### Simulate Trajectory
+
+# ...
+path_planner.simulate_trajectory( 0, np.array([0,0]) )
